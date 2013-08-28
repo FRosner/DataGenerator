c2612bd4359b5b39df10e20f867f85ef9109ef78
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-08-28 17:04:12 +0200

    #2 create standard Maven project

---
+++ src/main/java/de/frosner/datagenerator/App.java
@@ -0,0 +1,13 @@
+package de.frosner.datagenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
