05ccaf9cf6ac3422dd0bc982dac0f6c127eb4847
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-08-29 00:16:57 +0200

    #9 add another test to create more instances

--- src/test/java/de/frosner/datagenerator/main/DataGeneratorTest.java
+++ src/test/java/de/frosner/datagenerator/main/DataGeneratorTest.java
@@ -18,4 +18,21 @@ public class DataGeneratorTest {
 		mockedOut.addExpectedExport(new Instance(0, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE));
 		mockedOut.verify();
 	}
+
+	@Test
+	public void testGenerateInstances() {
+		int numberOfInstances = 5;
+		FeatureDefinition x = new FeatureDefinition("x", new DummyDistribution());
+		FeatureDefinition y = new FeatureDefinition("y", new DummyDistribution());
+		FeatureDefinition z = new FeatureDefinition("y", new DummyDistribution());
+		MockedExportConnection mockedOut = new MockedExportConnection();
+		DataGenerator generator = new DataGenerator(numberOfInstances, mockedOut, x, y, z);
+		generator.generate();
+		for (int i = 0; i < numberOfInstances; i++) {
+			mockedOut.addExpectedExport(new Instance(i, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE,
+					DummyDistribution.ANY_SAMPLE));
+		}
+		mockedOut.verify();
+	}
+
 }
