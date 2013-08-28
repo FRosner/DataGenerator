6a98cf328d65860daf7a3d7d652bf580cce4b86c
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-08-28 21:11:04 +0200

    #11 implement instances

---
+++ src/main/java/de/frosner/datagenerator/main/Instance.java
@@ -0,0 +1,30 @@
+package de.frosner.datagenerator.main;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import net.sf.qualitycheck.Check;
+
+import com.google.common.collect.ImmutableList;
+
+import de.frosner.datagenerator.features.FeatureValue;
+
+@Immutable
+public class Instance implements Iterable<FeatureValue> {
+
+	private final List<FeatureValue> _values;
+
+	public Instance(@Nonnull FeatureValue... values) {
+		Check.noNullElements(values, "All feature values must not be null.");
+		_values = ImmutableList.copyOf(values);
+	}
+
+	@Override
+	public Iterator<FeatureValue> iterator() {
+		return _values.listIterator();
+	}
+
+}
---
+++ src/test/java/de/frosner/datagenerator/main/InstanceTest.java
@@ -0,0 +1,28 @@
+package de.frosner.datagenerator.main;
+
+import static org.fest.assertions.Assertions.assertThat;
+import net.sf.qualitycheck.exception.IllegalNullElementsException;
+
+import org.junit.Test;
+
+import de.frosner.datagenerator.features.ContinuousFeatureValue;
+import de.frosner.datagenerator.features.FeatureValue;
+
+public class InstanceTest {
+
+	@Test(expected = IllegalNullElementsException.class)
+	public void testCreationWithNullArguments() {
+		new Instance(new ContinuousFeatureValue(0), null);
+	}
+
+	@Test
+	public void testIterator() {
+		Instance instance = new Instance(new ContinuousFeatureValue(1), new ContinuousFeatureValue(2),
+				new ContinuousFeatureValue(3));
+		int i = 1;
+		for (FeatureValue value : instance) {
+			assertThat(value).isEqualTo(new ContinuousFeatureValue(i++));
+		}
+	}
+
+}
