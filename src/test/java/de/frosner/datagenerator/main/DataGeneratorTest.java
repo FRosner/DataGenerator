f04523ea9e698852963924a97abfde08e9390654
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-08-29 19:23:01 +0200

    #9 add methods to create feature definitions and init the mocked export connection

--- src/test/java/de/frosner/datagenerator/main/DataGeneratorTest.java
+++ src/test/java/de/frosner/datagenerator/main/DataGeneratorTest.java
@@ -1,5 +1,6 @@
 package de.frosner.datagenerator.main;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.frosner.datagenerator.distributions.DummyDistribution;
@@ -8,31 +9,42 @@ import de.frosner.datagenerator.features.FeatureDefinition;
 
 public class DataGeneratorTest {
 
+	private MockedExportConnection _mockedOut;
+	private FeatureDefinition _x;
+	private FeatureDefinition _y;
+	private FeatureDefinition _z;
+	private DataGenerator _generator;
+
+	@Before
+	public void initMocks() {
+		_mockedOut = new MockedExportConnection();
+	}
+
+	@Before
+	public void createFeatureDefinitions() {
+		_x = new FeatureDefinition("x", new DummyDistribution());
+		_y = new FeatureDefinition("y", new DummyDistribution());
+		_z = new FeatureDefinition("z", new DummyDistribution());
+	}
+
 	@Test
 	public void testGenerateInstance() {
-		FeatureDefinition x = new FeatureDefinition("x", new DummyDistribution());
-		FeatureDefinition y = new FeatureDefinition("y", new DummyDistribution());
-		MockedExportConnection mockedOut = new MockedExportConnection();
-		DataGenerator generator = new DataGenerator(1, mockedOut, x, y);
-		generator.generate();
-		mockedOut.addExpectedExport(new Instance(0, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE));
-		mockedOut.verify();
+		_generator = new DataGenerator(1, _mockedOut, _x, _y);
+		_generator.generate();
+		_mockedOut.addExpectedExport(new Instance(0, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE));
+		_mockedOut.verify();
 	}
 
 	@Test
 	public void testGenerateInstances() {
 		int numberOfInstances = 5;
-		FeatureDefinition x = new FeatureDefinition("x", new DummyDistribution());
-		FeatureDefinition y = new FeatureDefinition("y", new DummyDistribution());
-		FeatureDefinition z = new FeatureDefinition("y", new DummyDistribution());
-		MockedExportConnection mockedOut = new MockedExportConnection();
-		DataGenerator generator = new DataGenerator(numberOfInstances, mockedOut, x, y, z);
-		generator.generate();
+		_generator = new DataGenerator(numberOfInstances, _mockedOut, _x, _y, _z);
+		_generator.generate();
 		for (int i = 0; i < numberOfInstances; i++) {
-			mockedOut.addExpectedExport(new Instance(i, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE,
+			_mockedOut.addExpectedExport(new Instance(i, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE,
 					DummyDistribution.ANY_SAMPLE));
 		}
-		mockedOut.verify();
+		_mockedOut.verify();
 	}
 
 }
