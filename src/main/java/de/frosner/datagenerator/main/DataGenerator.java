97b965cc6442c5662155cfb36f9dd1882b70fef7
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-08-30 17:45:03 +0200

    #23 document DataGenerator class

--- src/main/java/de/frosner/datagenerator/main/DataGenerator.java
+++ src/main/java/de/frosner/datagenerator/main/DataGenerator.java
@@ -14,23 +14,41 @@ import de.frosner.datagenerator.export.ExportConnection;
 import de.frosner.datagenerator.features.FeatureDefinition;
 import de.frosner.datagenerator.features.FeatureValue;
 
+/**
+ * Class for sampling a sequence of {@link Instance}s having the specified {@link FeatureDefinition}s. Sampled instances
+ * will be exported to the specified {@link ExportConnection}.
+ */
 public class DataGenerator {
 
 	private final int _numberOfInstances;
 	private final ExportConnection _out;
 	private final List<FeatureDefinition> _featureDefinitions;
 
-	public DataGenerator(int numberOfInstances, @Nonnull ExportConnection out,
+	/**
+	 * Constructs a new {@link DataGenerator} object. When {@link DataGenerator#generate()} is invoked it will sample
+	 * the specified number of instances with the specified feature definitions to the specified export connection.
+	 * 
+	 * @param numberOfInstances
+	 *            to be generated
+	 * @param exportConnection
+	 *            to export the generated instances to
+	 * @param featureDefinitions
+	 *            that contain the information for sampling the feature values of the instances
+	 */
+	public DataGenerator(int numberOfInstances, @Nonnull ExportConnection exportConnection,
 			@Nonnull FeatureDefinition... featureDefinitions) {
 		Check.stateIsTrue(numberOfInstances > 0, "Number of instances to generate must be > 0.");
-		Check.notNull(out);
+		Check.notNull(exportConnection);
 		Check.noNullElements(featureDefinitions);
 
 		_numberOfInstances = numberOfInstances;
-		_out = out;
+		_out = exportConnection;
 		_featureDefinitions = ImmutableList.copyOf(featureDefinitions);
 	}
 
+	/**
+	 * Sample and export instances to the corresponding {@link ExportConnection}.
+	 */
 	public void generate() {
 		for (int i = 0; i < _numberOfInstances; i++) {
 			List<FeatureValue> featureValues = Lists.newArrayList();
