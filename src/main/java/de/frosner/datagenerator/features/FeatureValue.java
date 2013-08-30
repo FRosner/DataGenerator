6653f4464182abd469ed7ceff4a8dd3679e46c63
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-08-30 17:36:30 +0200

    #23 document FeatureValue class

--- src/main/java/de/frosner/datagenerator/features/FeatureValue.java
+++ src/main/java/de/frosner/datagenerator/features/FeatureValue.java
@@ -2,9 +2,18 @@ package de.frosner.datagenerator.features;
 
 import javax.annotation.concurrent.Immutable;
 
+/**
+ * Wrapper for a feature value. It may contain a value of any type.
+ */
 @Immutable
 public interface FeatureValue {
 
+	/**
+	 * Returns the wrapped value. Call {@link Object#toString()} in order to get an exportable {@link String}
+	 * representation of the value.
+	 * 
+	 * @return
+	 */
 	public Object getValue();
 
 }
