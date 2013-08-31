2fee7eb68432a5d778d83f50fe624b7e69494ea9
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-08-31 15:56:48 +0200

    #23 document FeatureDefinition class

--- src/main/java/de/frosner/datagenerator/features/FeatureDefinition.java
+++ src/main/java/de/frosner/datagenerator/features/FeatureDefinition.java
@@ -5,22 +5,46 @@ import javax.annotation.concurrent.Immutable;
 
 import net.sf.qualitycheck.Check;
 import de.frosner.datagenerator.distributions.Distribution;
+import de.frosner.datagenerator.main.DataGenerator;
 
+/**
+ * This class contains all information about a feature, including the feature name and the underlying probability
+ * distribution. The {@link DataGenerator} uses feature definitions to sample individual {@link FeatureValue}s for each
+ * {@link Instance} generated.
+ */
 @Immutable
 public class FeatureDefinition {
 
 	private final String _name;
 	private final Distribution _distribution;
 
+	/**
+	 * Creates a new {@link FeatureDefinition} with the specified name and distribution.
+	 * 
+	 * @param name
+	 *            of the feature
+	 * @param distribution
+	 *            of the underlying feature values
+	 */
 	public FeatureDefinition(@Nonnull String name, @Nonnull Distribution distribution) {
 		_name = Check.notNull(name);
 		_distribution = Check.notNull(distribution);
 	}
 
+	/**
+	 * Returns the underlying probability distribution.
+	 * 
+	 * @return probability distribution for the feature values to be generated
+	 */
 	public Distribution getDistribution() {
 		return _distribution;
 	}
 
+	/**
+	 * Returns the name of the feature.
+	 * 
+	 * @return feature name
+	 */
 	public String getName() {
 		return _name;
 	}
