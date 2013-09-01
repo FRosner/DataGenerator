817e7091d5ac39bd5c5d301e771e0baab1789c90
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-09-01 14:22:16 +0200

    #20 replace custom mock verification with mockito

--- src/test/java/de/frosner/datagenerator/main/DataGeneratorTest.java
+++ src/test/java/de/frosner/datagenerator/main/DataGeneratorTest.java
@@ -1,5 +1,9 @@
 package de.frosner.datagenerator.main;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import net.sf.qualitycheck.exception.IllegalNullElementsException;
@@ -7,24 +11,26 @@ import net.sf.qualitycheck.exception.IllegalStateOfArgumentException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import com.google.common.collect.Lists;
 
 import de.frosner.datagenerator.distributions.DummyDistribution;
-import de.frosner.datagenerator.export.MockedExportConnection;
+import de.frosner.datagenerator.export.ExportConnection;
 import de.frosner.datagenerator.features.FeatureDefinition;
 
 public class DataGeneratorTest {
 
-	private MockedExportConnection _mockedOut;
+	@Mock
+	private ExportConnection _mockedOut;
 	private FeatureDefinition _x;
 	private FeatureDefinition _y;
 	private FeatureDefinition _z;
 	private DataGenerator _generator;
 
 	@Before
-	public void initMocks() {
-		_mockedOut = new MockedExportConnection();
+	public void setUp() {
+		initMocks(this);
 	}
 
 	@Before
@@ -60,23 +66,24 @@ public class DataGeneratorTest {
 	}
 
 	@Test
-	public void testGenerateInstance() {
+	public void testGenerateInstance() throws IOException {
 		_generator = new DataGenerator(1, _mockedOut, _x, _y);
 		_generator.generate();
-		_mockedOut.addExpectedExport(new Instance(0, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE));
-		_mockedOut.verify();
+		verify(_mockedOut).export(new Instance(0, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE));
+		verify(_mockedOut).close();
 	}
 
 	@Test
-	public void testGenerateInstances() {
+	public void testGenerateInstances() throws IOException {
 		int numberOfInstances = 5;
 		_generator = new DataGenerator(numberOfInstances, _mockedOut, _x, _y, _z);
 		_generator.generate();
 		for (int i = 0; i < numberOfInstances; i++) {
-			_mockedOut.addExpectedExport(new Instance(i, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE,
-					DummyDistribution.ANY_SAMPLE));
+			verify(_mockedOut).export(
+					new Instance(i, DummyDistribution.ANY_SAMPLE, DummyDistribution.ANY_SAMPLE,
+							DummyDistribution.ANY_SAMPLE));
 		}
-		_mockedOut.verify();
+		verify(_mockedOut).close();
 	}
 
 }
