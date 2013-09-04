e2eeadaf74bfb85c87d146832160048adbc3c5ef
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-09-04 21:20:44 +0200

    #25 add data generation input mask

--- src/main/java/de/frosner/datagenerator/gui/SwingLauncher.java
+++ src/main/java/de/frosner/datagenerator/gui/SwingLauncher.java
@@ -4,17 +4,16 @@ import javax.swing.SwingUtilities;
 
 public final class SwingLauncher {
 
-	/**
-	 * @param args
-	 */
+	public static SwingMenu GUI; 
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				SwingMenu ex = new SwingMenu();
-				ex.setVisible(true);
+				GUI = new SwingMenu();
+				GUI.setVisible(true);
 			}
 		});
 	}
-
+	
 }
--- src/main/java/de/frosner/datagenerator/gui/SwingMenu.java
+++ src/main/java/de/frosner/datagenerator/gui/SwingMenu.java
@@ -1,8 +1,10 @@
 package de.frosner.datagenerator.gui;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
@@ -16,10 +18,11 @@ public final class SwingMenu extends JFrame {
 
 	private static final int BUTTON_HEIGHT = 25;
 	private static final int LABEL_HEIGHT = 25;
-	private static final int LABEL_WIDTH = 50;
+	private static final int LABEL_WIDTH = 100;
 	private static final int TEXT_FIELD_HEIGHT = 25;
-	private static final int TEXT_FIELD_WIDTH = 150;
+	private static final int TEXT_FIELD_WIDTH = 180;
 	private static final int ROW_MARGIN = 15;
+	private static final int SECTION_MARGIN = 50;
 	private static final int PANEL_HEIGHT = 500;
 	private static final int PANEL_WIDTH = 500;
 
@@ -36,6 +39,15 @@ public final class SwingMenu extends JFrame {
 	private final JLabel _gaussianSigmaLabel = new JLabel("Sigma", JLabel.RIGHT);
 	private final JTextField _gaussianSigmaField = new JTextField();
 
+	private final JLabel _numberOfInstancesLabel = new JLabel("#Instances", JLabel.RIGHT);
+	private final JTextField _numberOfInstancesField = new JTextField();
+	private final JLabel _exportFileLabel = new JLabel("Export File", JLabel.RIGHT);
+	private final JFileChooser _exportFileDialog = new JFileChooser();
+	private final JButton _exportFileButton = new JButton(".");
+	private final JTextField _exportFileField = new JTextField();
+
+	private final JList _featureList = new JList(new Object[] { "Feature 1", "Feature 2", "Feature 3" });
+
 	public SwingMenu() {
 		initUi();
 		initPanel();
@@ -44,15 +56,22 @@ public final class SwingMenu extends JFrame {
 
 		HorizontalRuler.alignBottomAt(TEXT_FIELD_HEIGHT + ROW_MARGIN, _gaussianNameLabel, _gaussianNameField);
 		HorizontalRuler.alignBottomAt(2 * (TEXT_FIELD_HEIGHT + ROW_MARGIN), _gaussianMeanLabel, _gaussianMeanField);
-		HorizontalRuler.alignBottomAt(3 * (TEXT_FIELD_HEIGHT + ROW_MARGIN), _gaussianSigmaLabel, _gaussianSigmaField);
-		VerticalRuler.alignRightAt(50, _gaussianNameLabel, _gaussianMeanLabel, _gaussianSigmaLabel);
+		HorizontalRuler.alignBottomAt(3 * (TEXT_FIELD_HEIGHT + ROW_MARGIN), _gaussianSigmaLabel, _gaussianSigmaField,
+				_featureList);
+		VerticalRuler.alignRightAt(75, _gaussianNameLabel, _gaussianMeanLabel, _gaussianSigmaLabel,
+				_numberOfInstancesLabel, _exportFileLabel);
 
 		HorizontalRuler.alignBottomAt(4 * (TEXT_FIELD_HEIGHT + ROW_MARGIN), _addFeatureButton, _removeFeatureButton);
-		VerticalRuler.alignRightAt(210, _addFeatureButton, _generateDataButton, _gaussianNameField, _gaussianMeanField,
-				_gaussianSigmaField);
+		VerticalRuler.alignRightAt(260, _addFeatureButton, _generateDataButton, _gaussianNameField, _gaussianMeanField,
+				_gaussianSigmaField, _exportFileButton, _numberOfInstancesField);
+		VerticalRuler.alignRightAt(_exportFileButton.getX(), _exportFileField);
 
-		HorizontalRuler.alignBottomAt(300, _generateDataButton);
-		VerticalRuler.alignRightAt(450, _removeFeatureButton);
+		VerticalRuler.alignRightAt(PANEL_WIDTH - 40, _removeFeatureButton, _featureList);
+		HorizontalRuler.alignBottomAt(5 * (TEXT_FIELD_HEIGHT + ROW_MARGIN) + SECTION_MARGIN, _numberOfInstancesField,
+				_numberOfInstancesLabel);
+		HorizontalRuler.alignBottomAt(6 * (TEXT_FIELD_HEIGHT + ROW_MARGIN) + SECTION_MARGIN, _exportFileLabel,
+				_exportFileButton, _exportFileField);
+		HorizontalRuler.alignBottomAt(7 * (TEXT_FIELD_HEIGHT + ROW_MARGIN) + SECTION_MARGIN, _generateDataButton);
 	}
 
 	private void initUi() {
@@ -74,11 +93,13 @@ public final class SwingMenu extends JFrame {
 
 	private void initFeatureSection() {
 		initGaussianFeatureMask();
+		initFeatureList();
 		initAddFeatureButton();
 		initRemoveFeatureButton();
 	}
 
 	private void initGenerationSection() {
+		initGenerationMask();
 		initGenerateDataButton();
 	}
 
@@ -95,7 +116,11 @@ public final class SwingMenu extends JFrame {
 		_gaussianSigmaField.setBounds(-1, -1, TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT);
 		_panel.add(_gaussianSigmaLabel);
 		_panel.add(_gaussianSigmaField);
+	}
 
+	private void initFeatureList() {
+		_featureList.setBounds(-1, -1, TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT * 3 + ROW_MARGIN * 2);
+		_panel.add(_featureList);
 	}
 
 	private void initAddFeatureButton() {
@@ -114,12 +139,32 @@ public final class SwingMenu extends JFrame {
 		_panel.add(_removeFeatureButton);
 	}
 
+	private void initGenerationMask() {
+		_numberOfInstancesLabel.setBounds(-1, -1, LABEL_WIDTH, LABEL_HEIGHT);
+		_numberOfInstancesField.setBounds(-1, -1, TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT);
+		_exportFileLabel.setBounds(-1, -1, LABEL_WIDTH, LABEL_HEIGHT);
+		_exportFileButton.setBounds(-1, -1, TEXT_FIELD_HEIGHT, TEXT_FIELD_HEIGHT);
+		_exportFileButton.addActionListener(new ExportFileButtonActionListener());
+		_exportFileField.setBounds(-1, -1, TEXT_FIELD_WIDTH - TEXT_FIELD_HEIGHT, TEXT_FIELD_HEIGHT);
+		_panel.add(_numberOfInstancesLabel);
+		_panel.add(_numberOfInstancesField);
+		_panel.add(_exportFileLabel);
+		_panel.add(_exportFileButton);
+		_panel.add(_exportFileField);
+	}
+
 	private void initGenerateDataButton() {
 		int width = 140;
 		int height = BUTTON_HEIGHT;
 		_generateDataButton.setBounds(-1, -1, width, height);
 		_generateDataButton.addActionListener(new GenerateDataButtonActionListener());
 		_panel.add(_generateDataButton);
+	}
+
+	public void openExportFileChooserDialog() {
+		if (_exportFileDialog.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+			_exportFileField.setText(_exportFileDialog.getSelectedFile().getName());
+		}
 	}
 
 }
