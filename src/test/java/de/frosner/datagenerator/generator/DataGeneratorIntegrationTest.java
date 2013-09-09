4046f743255c29e1d950706844fed69d210ca116
Husterknupp <4.23@gmx.de>
2013-09-09 13:17:49 +0200

    #67 Fixed regex, so it matches a double containing an exponent as well

--- src/test/java/de/frosner/datagenerator/generator/DataGeneratorIntegrationTest.java
+++ src/test/java/de/frosner/datagenerator/generator/DataGeneratorIntegrationTest.java
@@ -12,7 +12,6 @@ import de.frosner.datagenerator.distributions.GaussianDistribution;
 import de.frosner.datagenerator.export.CsvExportConnection;
 import de.frosner.datagenerator.export.ExportConnection;
 import de.frosner.datagenerator.features.FeatureDefinition;
-import de.frosner.datagenerator.generator.DataGenerator;
 
 public class DataGeneratorIntegrationTest {
 
@@ -38,14 +37,16 @@ public class DataGeneratorIntegrationTest {
 		_exportConnection = new CsvExportConnection(_out);
 		_fd1 = new FeatureDefinition("fd1_hightUsMen", new GaussianDistribution(177.8, 7.62));
 		_fd2 = new FeatureDefinition("fd2_hightDogs", new GaussianDistribution(394, 147.32));
-		_fd3 = new FeatureDefinition("fd3_pirateGold", new GaussianDistribution(5, 2.24));
+		_fd3 = new FeatureDefinition("fd3_pirateGold", new GaussianDistribution(5.4234324235235345E-14,
+				2.243847294729784E-16));
 		_fd4 = new FeatureDefinition("fd4_random1", new GaussianDistribution(-9, 1));
-		_fd5 = new FeatureDefinition("fd5_temperatureMallorca", new GaussianDistribution(300, 5));
+		_fd5 = new FeatureDefinition("fd5_temperatureMallorca", new GaussianDistribution(5.4234324235235345E+14,
+				2.243847294729784E+16));
 		_dataGenerator = new DataGenerator(_numberOfInstances, _exportConnection, _fd1, _fd2, _fd3, _fd4, _fd5);
 
 		_dataGenerator.generate();
 
-		String regex = "^\\-?[0-9]+\\.[0-9]+$";
+		String regex = "^\\-?[0-9]+\\.[0-9]+(E\\-?[0-9]+)?$";
 		String[] lines = _out.toString().split("\\n");
 
 		assertThat(lines).hasSize(_numberOfInstances);
