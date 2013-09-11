b2f6a300ccfe04a80c9dc45ddf613d4041da913c
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-09-11 18:02:24 +0200

    #88 adjust version

--- src/main/java/de/frosner/datagenerator/util/ApplicationMetaData.java
+++ src/main/java/de/frosner/datagenerator/util/ApplicationMetaData.java
@@ -2,7 +2,7 @@ package de.frosner.datagenerator.util;
 
 public final class ApplicationMetaData {
 
-	public static final String VERSION = "v0.2";
+	public static final String VERSION = "v0.2.1";
 	public static final String APPLICATION_NAME = "Data Generator";
 	public static final int SERIAL_VERSION_UID = VERSION.hashCode();
 
