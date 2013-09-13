f796920dd47363ade4b88a6b0750fd924fc72c46
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-09-13 20:37:13 +0200

    #78 document methods

--- src/main/java/de/frosner/datagenerator/gui/verifiers/InputVerifier.java
+++ src/main/java/de/frosner/datagenerator/gui/verifiers/InputVerifier.java
@@ -8,6 +8,12 @@ import org.apache.commons.lang.StringUtils;
 
 import de.frosner.datagenerator.util.VisibleForTesting;
 
+/**
+ * Class to verify text input and change the color of a specified component when the input cannot be verified. It is
+ * used much like a builder but instead of building it verifies the input given the conditions selected.
+ * <p>
+ * Example: {@code InputVerifier.isNumber("14").isPositive().verify()} returns {@code true}.
+ */
 public abstract class InputVerifier {
 
 	@VisibleForTesting
@@ -21,10 +27,29 @@ public abstract class InputVerifier {
 		_verified = isVerified;
 	}
 
+	/**
+	 * Returns whether the input could be verified given the specified conditions.
+	 * 
+	 * @return whether the input could be verified
+	 */
 	public boolean verify() {
 		return _verified;
 	}
 
+	/**
+	 * This method is used to change the color of a component if verification was not successful. This will enable the
+	 * user to see where he did a mistake. The verification boolean is returned to be used by calling control sequence
+	 * as well.
+	 * <p>
+	 * Example: {@code InputVerifier.verifyComponent(myTextArea, InputVerifier.isDouble(myTextArea.getText()).verify())}
+	 * will change the color of the text area if the entered text cannot be parsed to a {@code Double}.
+	 * 
+	 * @param component
+	 *            to change the color of in case of unsuccessful verification
+	 * @param verification
+	 *            is a boolean indicating whether the verification was successful
+	 * @return <b>verification</b>
+	 */
 	public static boolean verifyComponent(JComponent component, boolean verification) {
 		if (verification) {
 			component.setBackground(VALID_INPUT_WHITE);
@@ -34,6 +59,12 @@ public abstract class InputVerifier {
 		return verification;
 	}
 
+	/**
+	 * Verifies whether the specified input text can be parsed to an {@code Integer}.
+	 * 
+	 * @param input
+	 *            to verify
+	 */
 	public static IntegerVerifier isInteger(String input) {
 		try {
 			return new IntegerVerifier(Integer.parseInt(input));
@@ -42,6 +73,12 @@ public abstract class InputVerifier {
 		}
 	}
 
+	/**
+	 * Verifies whether the specified input text can be parsed to a {@code Double} that is not infinite or NaN.
+	 * 
+	 * @param input
+	 *            to verify
+	 */
 	public static DoubleVerifier isDouble(String input) {
 		try {
 			Double doubleInput = Double.parseDouble(input);
@@ -54,6 +91,12 @@ public abstract class InputVerifier {
 		}
 	}
 
+	/**
+	 * Verifies whether the specified input text can be used as a name. That is it must not be blank.
+	 * 
+	 * @param input
+	 *            to verify
+	 */
 	public static NameVerifier isName(String input) {
 		if (StringUtils.isNotBlank(input)) {
 			return new NameVerifier(input);
