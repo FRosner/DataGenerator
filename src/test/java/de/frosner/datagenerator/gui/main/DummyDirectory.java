87eb173d7c6139de0fe78f9c8847d56f74704b10
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-02 11:51:09 +0200

    #121 add serial uid

--- src/test/java/de/frosner/datagenerator/gui/main/DummyDirectory.java
+++ src/test/java/de/frosner/datagenerator/gui/main/DummyDirectory.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 public class DummyDirectory extends File {
 
+	private static final long serialVersionUID = 1L;
+
 	public DummyDirectory(String dirName) {
 		super(dirName);
 	}
