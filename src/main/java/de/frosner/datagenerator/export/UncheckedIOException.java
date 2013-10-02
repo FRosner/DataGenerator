e2430cf43be9cd10f7219e436458abe3d0a82e9c
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-02 23:18:04 +0200

    #123 finalize UncheckedIOException

--- src/main/java/de/frosner/datagenerator/export/UncheckedIOException.java
+++ src/main/java/de/frosner/datagenerator/export/UncheckedIOException.java
@@ -2,7 +2,7 @@ package de.frosner.datagenerator.export;
 
 import de.frosner.datagenerator.util.ApplicationMetaData;
 
-public class UncheckedIOException extends RuntimeException {
+public final class UncheckedIOException extends RuntimeException {
 
 	private static final long serialVersionUID = ApplicationMetaData.SERIAL_VERSION_UID;
 
