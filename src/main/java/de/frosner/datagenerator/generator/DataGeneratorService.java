af96844af26215805c9de87d310a45468ac46750
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-02 12:05:42 +0200

    #112 document DataGeneratorService

--- src/main/java/de/frosner/datagenerator/generator/DataGeneratorService.java
+++ src/main/java/de/frosner/datagenerator/generator/DataGeneratorService.java
@@ -15,6 +15,12 @@ import de.frosner.datagenerator.features.FeatureDefinition;
 import de.frosner.datagenerator.gui.main.TextAreaLogger;
 import de.frosner.datagenerator.util.VisibleForTesting;
 
+/**
+ * Service maintaining a list of {@link FeatureDefinition} instances. It can be used to generate data to a specified
+ * export connection.
+ * <p>
+ * The feature definition list should be kept synchronized with the list in the UI.
+ */
 public final class DataGeneratorService {
 
 	public static final DataGeneratorService INSTANCE = new DataGeneratorService();
@@ -27,15 +33,35 @@ public final class DataGeneratorService {
 	DataGeneratorService() {
 	}
 
+	/**
+	 * Adds a feature definition to the list. Make sure that it is added to the list in the UI as well.
+	 * 
+	 * @param featureDefinition
+	 *            to add
+	 */
 	public void addFeatureDefinition(FeatureDefinition featureDefinition) {
 		_featureDefinitions.add(featureDefinition);
 		TextAreaLogger.info("Added Feature: " + featureDefinition.getName());
 	}
 
+	/**
+	 * Removes a feature definition from the list at the specified index. Make sure that it is removed from the list in
+	 * the UI as well.
+	 * 
+	 * @param index
+	 */
 	public void removeFeatureDefinition(int index) {
 		TextAreaLogger.info("Removed Feature: " + _featureDefinitions.remove(index).getName());
 	}
 
+	/**
+	 * Generate and export a number of instances to a CSV file.
+	 * 
+	 * @param numberOfInstances
+	 *            to be generated
+	 * @param exportConfig
+	 *            containing file and options
+	 */
 	public void generateData(int numberOfInstances, CsvExportConfiguration exportConfig) {
 		File exportFile = exportConfig.getFile();
 		if (!_generating) {
