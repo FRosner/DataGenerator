e3f6c1d06fd4942c1cfd023b633dc00622808613
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-02 23:17:43 +0200

    #123 finalize IllegalMethodCallSequenceException

--- src/main/java/de/frosner/datagenerator/export/IllegalMethodCallSequenceException.java
+++ src/main/java/de/frosner/datagenerator/export/IllegalMethodCallSequenceException.java
@@ -2,7 +2,7 @@ package de.frosner.datagenerator.export;
 
 import de.frosner.datagenerator.util.ApplicationMetaData;
 
-public class IllegalMethodCallSequenceException extends RuntimeException {
+public final class IllegalMethodCallSequenceException extends RuntimeException {
 
 	private static final long serialVersionUID = ApplicationMetaData.SERIAL_VERSION_UID;
 
