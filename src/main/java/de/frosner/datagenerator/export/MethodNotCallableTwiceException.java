4915b390f0cd62da35e00e0983fb772143e71610
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-02 23:17:56 +0200

    #123 finalize MethodNotCallableTwiceException

--- src/main/java/de/frosner/datagenerator/export/MethodNotCallableTwiceException.java
+++ src/main/java/de/frosner/datagenerator/export/MethodNotCallableTwiceException.java
@@ -2,7 +2,7 @@ package de.frosner.datagenerator.export;
 
 import de.frosner.datagenerator.util.ApplicationMetaData;
 
-public class MethodNotCallableTwiceException extends RuntimeException {
+public final class MethodNotCallableTwiceException extends RuntimeException {
 
 	private static final long serialVersionUID = ApplicationMetaData.SERIAL_VERSION_UID;
 
