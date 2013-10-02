ae2ad2868bed6f576183996d558c1c3559805c95
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-02 23:17:04 +0200

    #130 minor javadoc change

--- src/main/java/de/frosner/datagenerator/util/VisibleForTesting.java
+++ src/main/java/de/frosner/datagenerator/util/VisibleForTesting.java
@@ -1,10 +1,7 @@
 package de.frosner.datagenerator.util;
 
 /**
- * <p>
  * The following member is package private in order to be accessable by a unit test.
- * </p>
- * 
  */
 public @interface VisibleForTesting {
 
