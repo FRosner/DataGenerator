0276dab4b68c8c05ae1cb5595d5288cdaa8dc789
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-02 12:12:18 +0200

    #112 document ProgressBarManager

--- src/main/java/de/frosner/datagenerator/gui/main/ProgressBarManager.java
+++ src/main/java/de/frosner/datagenerator/gui/main/ProgressBarManager.java
@@ -3,6 +3,9 @@ package de.frosner.datagenerator.gui.main;
 import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 
+/**
+ * Service managing a {@link JProgressBar}. It can be used to increase or reset progress.
+ */
 public class ProgressBarManager {
 
 	private static JProgressBar _progressBar;
@@ -11,10 +14,19 @@ public class ProgressBarManager {
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * Set the progress bar to be managed.
+	 * 
+	 * @param progressBar
+	 *            to be managed.
+	 */
 	public static void setProgressBar(JProgressBar progressBar) {
 		_progressBar = progressBar;
 	}
 
+	/**
+	 * Increase the value of the progress bar by one.
+	 */
 	public static void increaseProgress() {
 		if (_progressBar != null) {
 			SwingUtilities.invokeLater(new Runnable() {
@@ -27,6 +39,9 @@ public class ProgressBarManager {
 		}
 	}
 
+	/**
+	 * Set the value of the progress bar to maximum.
+	 */
 	public static void setProgressToMaximum() {
 		if (_progressBar != null) {
 			SwingUtilities.invokeLater(new Runnable() {
@@ -39,6 +54,9 @@ public class ProgressBarManager {
 		}
 	}
 
+	/**
+	 * Reset progress of the progress bar.
+	 */
 	public static void resetProgress() {
 		if (_progressBar != null) {
 			SwingUtilities.invokeLater(new Runnable() {
