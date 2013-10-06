cbd433520012b18dcd2a27fdeef18a31110a40e7
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-06 11:44:43 +0200

    #130 document ExportConfiguration

--- src/main/java/de/frosner/datagenerator/export/ExportConfiguration.java
+++ src/main/java/de/frosner/datagenerator/export/ExportConfiguration.java
@@ -1,7 +1,17 @@
 package de.frosner.datagenerator.export;
 
+/**
+ * Interface representing a configuration that can be used to create an {@link ExportConnection}. Configurations are
+ * unfinished connections created by the front end and handed to a service. The service then creates and opens the
+ * corresponding {@link ExportConnection}. This avoids connection handling in the front end thread.
+ */
 public interface ExportConfiguration {
 
+	/**
+	 * Create the corresponding {@link ExportConnection} from the current {@link ExportConfiguration}.
+	 * 
+	 * @return {@link ExportConnection} created
+	 */
 	public ExportConnection createExportConnection();
 
 }
