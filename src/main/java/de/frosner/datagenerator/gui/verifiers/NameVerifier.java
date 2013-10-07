48a333099df06df543ed6439df3ec689f5154f3e
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-07 18:25:51 +0200

    #130 document NameVerifier

--- src/main/java/de/frosner/datagenerator/gui/verifiers/NameVerifier.java
+++ src/main/java/de/frosner/datagenerator/gui/verifiers/NameVerifier.java
@@ -1,5 +1,8 @@
 package de.frosner.datagenerator.gui.verifiers;
 
+/**
+ * {@linkplain InputVerifier} to verify that a given text satisfies certain conditions.
+ */
 public final class NameVerifier extends InputVerifier {
 
 	private String _name;
@@ -15,6 +18,12 @@ public final class NameVerifier extends InputVerifier {
 		_name = string;
 	}
 
+	/**
+	 * Verify that the text is not longer than the given size.
+	 * 
+	 * @param size
+	 *            not to be exceeded
+	 */
 	public NameVerifier isNotLongerThan(int size) {
 		if (_verified) {
 			_verified = _name.length() <= size;
@@ -22,6 +31,9 @@ public final class NameVerifier extends InputVerifier {
 		return this;
 	}
 
+	/**
+	 * Verify that the text could be a valid file name.
+	 */
 	public NameVerifier isFileName() {
 		if (_verified) {
 			_verified = !_name.startsWith(" ") && !_name.endsWith(" ");
