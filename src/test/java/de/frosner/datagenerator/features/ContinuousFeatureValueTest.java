ef8bd77b8d3589f9e1a50ecb6dcf23ad3672e99d
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-18 23:58:32 +0200

    #41 extend continuous feature value test

--- src/test/java/de/frosner/datagenerator/features/ContinuousFeatureValueTest.java
+++ src/test/java/de/frosner/datagenerator/features/ContinuousFeatureValueTest.java
@@ -2,13 +2,32 @@ package de.frosner.datagenerator.features;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ContinuousFeatureValueTest {
 
+	private ContinuousFeatureValue _value;
+
+	@Before
+	public void createFeatureValue() {
+		_value = new ContinuousFeatureValue(0);
+	}
+
 	@Test
 	public void testEquals() {
-		assertThat(new ContinuousFeatureValue(0)).isEqualTo(new ContinuousFeatureValue(0));
-		assertThat(new ContinuousFeatureValue(0)).isNotEqualTo(new ContinuousFeatureValue(1));
+		assertThat(_value).isEqualTo(new ContinuousFeatureValue(0));
+		assertThat(_value).isNotEqualTo(new ContinuousFeatureValue(1));
 	}
+
+	@Test
+	public void testGetValue() {
+		assertThat(_value.getValue()).isEqualTo(0D);
+	}
+
+	@Test
+	public void testGetValueAsString() {
+		assertThat(_value.getValueAsString()).isEqualTo("0.0");
+	}
+
 }
