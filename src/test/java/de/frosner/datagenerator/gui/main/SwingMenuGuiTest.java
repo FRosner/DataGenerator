224e704cae57d6e788546c834d4131b41ddcaf0a
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-11-06 19:22:03 +0100

    #119 extend integration tests

--- src/test/java/de/frosner/datagenerator/gui/main/SwingMenuGuiTest.java
+++ src/test/java/de/frosner/datagenerator/gui/main/SwingMenuGuiTest.java
@@ -234,7 +234,47 @@ public class SwingMenuGuiTest {
 	}
 
 	@Test
-	public void testEditFeature() {
+	public void testEditFeature_gaussian() {
+		assertThat(_frame._featureListModel.getSize()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
+		_frameTestUtil.enterText(_frame._gaussianMeanField, "0");
+		_frameTestUtil.enterText(_frame._gaussianSigmaField, "1.0");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, SelectableDistribution.GAUSSIAN);
+		_frameTestUtil.addEnteredFeature();
+		_frameTestUtil.delay(500);
+		_frameTestUtil.selectFeature(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
+		_frameTestUtil.enterText(_frame._gaussianMeanField, "-1000");
+		_frameTestUtil.enterText(_frame._gaussianSigmaField, "2");
+		_frameTestUtil.updateSelectedFeature();
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureListModel.getSize()).isEqualTo(1);
+		assertThat(_frame._featureListModel.get(0)).isEqualTo("NewFeatureName (Gaussian, Mean = -1000.0, Sigma = 2.0)");
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^\\-[0-9]+.*$");
+	}
+
+	@Test
+	public void testEditFeature_bernoulli() {
+		assertThat(_frame._featureListModel.getSize()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
+		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "0.0");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, SelectableDistribution.BERNOULLI);
+		_frameTestUtil.addEnteredFeature();
+		_frameTestUtil.delay(500);
+		_frameTestUtil.selectFeature(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
+		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "1.0");
+		_frameTestUtil.updateSelectedFeature();
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureListModel.getSize()).isEqualTo(1);
+		assertThat(_frame._featureListModel.get(0)).isEqualTo("NewFeatureName (Bernoulli, p = 1.0)");
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEqualTo("1");
+	}
+
+	@Test
+	public void testEditFeature_uniformCategorial() {
 		assertThat(_frame._featureListModel.getSize()).isEqualTo(0);
 		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
 		_frameTestUtil.enterText(_frame._uniformCategorialNumberOfStatesField, "1");
