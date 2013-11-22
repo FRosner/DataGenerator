d27717c3647289fa23002986d15a09e843c02e55
Benjamin Schandera <4.23@gmx.de>
2013-11-22 23:57:59 +0100

    #179 provide constants for title values

--- src/main/java/de/frosner/datagenerator/gui/main/FeatureDefinitionDialog.java
+++ src/main/java/de/frosner/datagenerator/gui/main/FeatureDefinitionDialog.java
@@ -8,6 +8,9 @@ import de.frosner.datagenerator.util.ApplicationMetaData;
 
 public class FeatureDefinitionDialog extends JDialog {
 
+	public static final String TITLE_ADD = "Add Feature";
+	public static final String TITLE_EDIT = "Edit Feature";
+
 	private static final long serialVersionUID = ApplicationMetaData.SERIAL_VERSION_UID;
 
 	private int _featureIndexToEdit = -1;
@@ -18,7 +21,7 @@ public class FeatureDefinitionDialog extends JDialog {
 
 	public void setFeatureToEdit(int featureIndexToEdit) {
 		_featureIndexToEdit = featureIndexToEdit;
-		setTitle("Edit Feature");
+		setTitle(TITLE_EDIT);
 	}
 
 	public int getFeatureToEdit() {
@@ -31,7 +34,7 @@ public class FeatureDefinitionDialog extends JDialog {
 
 	public void leaveEditMode() {
 		_featureIndexToEdit = -1;
-		setTitle("Add Feature");
+		setTitle(TITLE_ADD);
 	}
 
 }
