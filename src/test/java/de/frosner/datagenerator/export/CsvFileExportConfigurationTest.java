c51bca4e6c404de15e68e454af8403e2eb41e8de
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-12-12 00:16:06 +0100

    #203 rename CSV file export configuration test

--- src/test/java/de/frosner/datagenerator/export/CsvFileExportConfigurationTest.java
+++ src/test/java/de/frosner/datagenerator/export/CsvFileExportConfigurationTest.java
@@ -8,7 +8,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CsvExportConfigurationTest {
+public class CsvFileExportConfigurationTest {
 
 	private File _exportFile;
 
