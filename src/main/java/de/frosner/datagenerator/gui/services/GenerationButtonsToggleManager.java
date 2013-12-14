043f1ebeb849ac8dd1f654d6abcece17e055e72a
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-12-15 00:49:17 +0100

    #206 annotate checked arguments of GenerationButtonsToggleManager

--- src/main/java/de/frosner/datagenerator/gui/services/GenerationButtonsToggleManager.java
+++ src/main/java/de/frosner/datagenerator/gui/services/GenerationButtonsToggleManager.java
@@ -1,5 +1,6 @@
 package de.frosner.datagenerator.gui.services;
 
+import javax.annotation.Nonnull;
 import javax.swing.JButton;
 import javax.swing.SwingUtilities;
 
@@ -14,7 +15,7 @@ public class GenerationButtonsToggleManager {
 	private static JButton _button1;
 	private static JButton _button2;
 
-	public static void setButtons(JButton button1, JButton button2) {
+	public static void setButtons(@Nonnull JButton button1, @Nonnull JButton button2) {
 		_button1 = Check.notNull(button1);
 		_button2 = Check.notNull(button2);
 	}
