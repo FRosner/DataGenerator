c2a6b793f424a8a4cde19120eeb27e5a24825bd5
FRosner <frank.rosner@student.uni-halle.de>
2014-01-30 12:46:13 +0100

    #184 create separate test class for feature manipulation

---
+++ src/test/java/de/frosner/datagenerator/gui/main/SwingMenuFeatureManipulationIntegrationTest.java
@@ -0,0 +1,212 @@
+package de.frosner.datagenerator.gui.main;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.swing.edt.GuiActionRunner.execute;
+
+import java.awt.AWTException;
+import java.io.File;
+import java.util.List;
+
+import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.edt.GuiTask;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import de.frosner.datagenerator.distributions.BernoulliDistribution;
+import de.frosner.datagenerator.distributions.CategorialDistribution;
+import de.frosner.datagenerator.distributions.FixedParameter;
+import de.frosner.datagenerator.distributions.GaussianDistribution;
+import de.frosner.datagenerator.features.FeatureDefinition;
+import de.frosner.datagenerator.gui.services.DataGeneratorService;
+
+public class SwingMenuFeatureManipulationIntegrationTest {
+
+	private SwingMenu _frame;
+	private SwingMenuTestUtil _frameTestUtil;
+	private File _testFile = new File("src/test/resources/" + SwingMenuGuiTest.class.getSimpleName() + ".tmp");
+
+	@BeforeClass
+	public static void setUpOnce() {
+		FailOnThreadViolationRepaintManager.install();
+	}
+
+	@Before
+	public void setUp() throws AWTException {
+		DataGeneratorService.INSTANCE.reset();
+		_frame = GuiActionRunner.execute(new GuiQuery<SwingMenu>() {
+			@Override
+			protected SwingMenu executeInEDT() {
+				return new SwingMenu();
+			}
+		});
+		_frameTestUtil = new SwingMenuTestUtil(_frame);
+		SwingLauncher.GUI = _frame;
+		_frameTestUtil.setExportFileFilter(SwingMenu.ALL_FILE_FILTER);
+		if (_testFile.exists()) {
+			_testFile.delete();
+		}
+	}
+
+	@After
+	public void destroyGUI() {
+		execute(new GuiTask() {
+			@Override
+			public void executeInEDT() {
+				_frame.dispose();
+			}
+		});
+		SwingMenuTestUtil.resetComponentManagers();
+	}
+
+	@Test
+	public void testAddAndRemoveFeature_gaussian() {
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
+		_frameTestUtil.enterText(_frame._gaussianMeanField, "0");
+		_frameTestUtil.enterText(_frame._gaussianSigmaField, "1.0");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, GaussianFeatureEntry.KEY);
+		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
+				new GaussianFeatureEntry(new FeatureDefinition("Feature", new GaussianDistribution(
+						new FixedParameter<Double>(0d), new FixedParameter<Double>(1d))), "0", "1.0").toString());
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^\\-?[0-9]+.*$");
+		_frameTestUtil.selectFeatureDefinitionEntryByName("Feature");
+		_frameTestUtil.clickButtonOrItem(_frame._removeFeatureButton);
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEmpty();
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEmpty();
+	}
+
+	@Test
+	public void testAddAndRemoveFeature_bernoulli() {
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
+		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "1.0");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, BernoulliFeatureEntry.KEY);
+		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
+				new BernoulliFeatureEntry(new FeatureDefinition("Feature", new BernoulliDistribution(
+						new FixedParameter<Double>(1d))), "1.0").toString());
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEqualTo("1");
+		_frameTestUtil.selectFeatureDefinitionEntryByName("Feature");
+		_frameTestUtil.clickButtonOrItem(_frame._removeFeatureButton);
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEmpty();
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEmpty();
+	}
+
+	@Test
+	public void testAddAndRemoveFeature_uniformCategorial() {
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
+		_frameTestUtil.enterText(_frame._uniformCategorialNumberOfStatesField, "1");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, UniformCategorialFeatureEntry.KEY);
+		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
+				new UniformCategorialFeatureEntry(new FeatureDefinition("Feature", new CategorialDistribution(
+						new FixedParameter<List<Double>>(Lists.newArrayList(1D)))), "1").toString());
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEqualTo("0");
+		_frameTestUtil.selectFeatureDefinitionEntryByName("Feature");
+		_frameTestUtil.clickButtonOrItem(_frame._removeFeatureButton);
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEmpty();
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEmpty();
+	}
+
+	@Test
+	public void testAddFeatureWithMenuItem() {
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
+		_frameTestUtil.enterText(_frame._gaussianMeanField, "0");
+		_frameTestUtil.enterText(_frame._gaussianSigmaField, "1.0");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, GaussianFeatureEntry.KEY);
+		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureMenuItem);
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
+				new GaussianFeatureEntry(new FeatureDefinition("Feature", new GaussianDistribution(
+						new FixedParameter<Double>(0d), new FixedParameter<Double>(1d))), "0", "1.0").toString());
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^\\-?[0-9]+.*$");
+	}
+
+	@Test
+	public void testEditFeature_gaussian() {
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
+		_frameTestUtil.enterText(_frame._gaussianMeanField, "0");
+		_frameTestUtil.enterText(_frame._gaussianSigmaField, "1.0");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, GaussianFeatureEntry.KEY);
+		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
+		_frameTestUtil.delay(500);
+		_frameTestUtil.selectFeatureDefinitionEntryByName("OldFeatureName");
+		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
+		_frameTestUtil.enterText(_frame._gaussianMeanField, "-1000");
+		_frameTestUtil.enterText(_frame._gaussianSigmaField, "2");
+		_frameTestUtil.updateSelectedFeature();
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(1);
+		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
+				new GaussianFeatureEntry(new FeatureDefinition("NewFeatureName", new GaussianDistribution(
+						new FixedParameter<Double>(-1000d), new FixedParameter<Double>(2d))), "-1000", "2").toString());
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^\\-[0-9]+.*$");
+	}
+
+	@Test
+	public void testEditFeature_bernoulli() {
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
+		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "0.0");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, BernoulliFeatureEntry.KEY);
+		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
+		_frameTestUtil.delay(500);
+		_frameTestUtil.selectFeatureDefinitionEntryByName("OldFeatureName");
+		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
+		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "1.0");
+		_frameTestUtil.updateSelectedFeature();
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(1);
+		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
+				new BernoulliFeatureEntry(new FeatureDefinition("NewFeatureName", new BernoulliDistribution(
+						new FixedParameter<Double>(1d))), "1.0").toString());
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEqualTo("1");
+	}
+
+	@Test
+	public void testEditFeature_uniformCategorial() {
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
+		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
+		_frameTestUtil.enterText(_frame._uniformCategorialNumberOfStatesField, "1");
+		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, UniformCategorialFeatureEntry.KEY);
+		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
+		_frameTestUtil.delay(500);
+		_frameTestUtil.selectFeatureDefinitionEntryByName("OldFeatureName");
+		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
+		_frameTestUtil.enterText(_frame._uniformCategorialNumberOfStatesField, "2");
+		_frameTestUtil.updateSelectedFeature();
+		_frameTestUtil.delay(500);
+		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(1);
+		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
+				new UniformCategorialFeatureEntry(new FeatureDefinition("NewFeatureName", new CategorialDistribution(
+						new FixedParameter<List<Double>>(Lists.newArrayList(0.5, 0.5)))), "2").toString());
+		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
+		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^(0|1)$");
+	}
+
+}
--- src/test/java/de/frosner/datagenerator/gui/main/SwingMenuGuiTest.java
+++ src/test/java/de/frosner/datagenerator/gui/main/SwingMenuGuiTest.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -29,13 +28,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import com.google.common.collect.Lists;
-
-import de.frosner.datagenerator.distributions.BernoulliDistribution;
-import de.frosner.datagenerator.distributions.CategorialDistribution;
-import de.frosner.datagenerator.distributions.FixedParameter;
-import de.frosner.datagenerator.distributions.GaussianDistribution;
-import de.frosner.datagenerator.features.FeatureDefinition;
 import de.frosner.datagenerator.gui.services.DataGeneratorService;
 import de.frosner.datagenerator.gui.services.TextAreaLogManager;
 import de.frosner.datagenerator.testutils.LongRunningSwingTests;
@@ -78,151 +70,6 @@ public class SwingMenuGuiTest {
 			}
 		});
 		SwingMenuTestUtil.resetComponentManagers();
-	}
-
-	@Test
-	public void testAddAndRemoveFeature_gaussian() {
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
-		_frameTestUtil.enterText(_frame._gaussianMeanField, "0");
-		_frameTestUtil.enterText(_frame._gaussianSigmaField, "1.0");
-		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, GaussianFeatureEntry.KEY);
-		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
-				new GaussianFeatureEntry(new FeatureDefinition("Feature", new GaussianDistribution(
-						new FixedParameter<Double>(0d), new FixedParameter<Double>(1d))), "0", "1.0").toString());
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^\\-?[0-9]+.*$");
-		_frameTestUtil.selectFeatureDefinitionEntryByName("Feature");
-		_frameTestUtil.clickButtonOrItem(_frame._removeFeatureButton);
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEmpty();
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEmpty();
-	}
-
-	@Test
-	public void testAddAndRemoveFeature_bernoulli() {
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
-		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "1.0");
-		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, BernoulliFeatureEntry.KEY);
-		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
-				new BernoulliFeatureEntry(new FeatureDefinition("Feature", new BernoulliDistribution(
-						new FixedParameter<Double>(1d))), "1.0").toString());
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEqualTo("1");
-		_frameTestUtil.selectFeatureDefinitionEntryByName("Feature");
-		_frameTestUtil.clickButtonOrItem(_frame._removeFeatureButton);
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEmpty();
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEmpty();
-	}
-
-	@Test
-	public void testAddAndRemoveFeature_uniformCategorial() {
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
-		_frameTestUtil.enterText(_frame._uniformCategorialNumberOfStatesField, "1");
-		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, UniformCategorialFeatureEntry.KEY);
-		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
-				new UniformCategorialFeatureEntry(new FeatureDefinition("Feature", new CategorialDistribution(
-						new FixedParameter<List<Double>>(Lists.newArrayList(1D)))), "1").toString());
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEqualTo("0");
-		_frameTestUtil.selectFeatureDefinitionEntryByName("Feature");
-		_frameTestUtil.clickButtonOrItem(_frame._removeFeatureButton);
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEmpty();
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEmpty();
-	}
-
-	@Test
-	public void testAddFeatureWithMenuItem() {
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		_frameTestUtil.enterText(_frame._featureNameField, "Feature");
-		_frameTestUtil.enterText(_frame._gaussianMeanField, "0");
-		_frameTestUtil.enterText(_frame._gaussianSigmaField, "1.0");
-		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, GaussianFeatureEntry.KEY);
-		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureMenuItem);
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
-				new GaussianFeatureEntry(new FeatureDefinition("Feature", new GaussianDistribution(
-						new FixedParameter<Double>(0d), new FixedParameter<Double>(1d))), "0", "1.0").toString());
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("Feature");
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^\\-?[0-9]+.*$");
-	}
-
-	@Test
-	public void testEditFeature_gaussian() {
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
-		_frameTestUtil.enterText(_frame._gaussianMeanField, "0");
-		_frameTestUtil.enterText(_frame._gaussianSigmaField, "1.0");
-		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, GaussianFeatureEntry.KEY);
-		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
-		_frameTestUtil.delay(500);
-		_frameTestUtil.selectFeatureDefinitionEntryByName("OldFeatureName");
-		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
-		_frameTestUtil.enterText(_frame._gaussianMeanField, "-1000");
-		_frameTestUtil.enterText(_frame._gaussianSigmaField, "2");
-		_frameTestUtil.updateSelectedFeature();
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(1);
-		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
-				new GaussianFeatureEntry(new FeatureDefinition("NewFeatureName", new GaussianDistribution(
-						new FixedParameter<Double>(-1000d), new FixedParameter<Double>(2d))), "-1000", "2").toString());
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^\\-[0-9]+.*$");
-	}
-
-	@Test
-	public void testEditFeature_bernoulli() {
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
-		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "0.0");
-		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, BernoulliFeatureEntry.KEY);
-		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
-		_frameTestUtil.delay(500);
-		_frameTestUtil.selectFeatureDefinitionEntryByName("OldFeatureName");
-		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
-		_frameTestUtil.enterText(_frame._bernoulliProbabilityField, "1.0");
-		_frameTestUtil.updateSelectedFeature();
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(1);
-		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
-				new BernoulliFeatureEntry(new FeatureDefinition("NewFeatureName", new BernoulliDistribution(
-						new FixedParameter<Double>(1d))), "1.0").toString());
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).isEqualTo("1");
-	}
-
-	@Test
-	public void testEditFeature_uniformCategorial() {
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(0);
-		_frameTestUtil.enterText(_frame._featureNameField, "OldFeatureName");
-		_frameTestUtil.enterText(_frame._uniformCategorialNumberOfStatesField, "1");
-		_frameTestUtil.selectOption(_frame._addFeatureDistributionSelection, UniformCategorialFeatureEntry.KEY);
-		_frameTestUtil.tryToAddEnteredFeature(_frame._addFeatureButton);
-		_frameTestUtil.delay(500);
-		_frameTestUtil.selectFeatureDefinitionEntryByName("OldFeatureName");
-		_frameTestUtil.enterText(_frame._featureNameField, "NewFeatureName");
-		_frameTestUtil.enterText(_frame._uniformCategorialNumberOfStatesField, "2");
-		_frameTestUtil.updateSelectedFeature();
-		_frameTestUtil.delay(500);
-		assertThat(_frame._featureGraph.getModel().getRootCount()).isEqualTo(1);
-		assertThat(_frame._featureGraph.getModel().getRootAt(0).toString()).isEqualTo(
-				new UniformCategorialFeatureEntry(new FeatureDefinition("NewFeatureName", new CategorialDistribution(
-						new FixedParameter<List<Double>>(Lists.newArrayList(0.5, 0.5)))), "2").toString());
-		assertThat((String) _frame._previewTableModel.getValueAt(0, 0)).isEqualTo("NewFeatureName");
-		assertThat((String) _frame._previewTableModel.getValueAt(1, 0)).matches("^(0|1)$");
 	}
 
 	@Test
