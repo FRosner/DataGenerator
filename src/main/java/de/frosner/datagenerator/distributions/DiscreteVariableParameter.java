3d3f41633f77922ebee62a1afcd9ca913a9a1109
Frank Rosner <frank.rosner@student.uni-halle.de>
2014-02-11 00:55:35 +0100

    #213 minor refactoring (rename variable)

--- src/main/java/de/frosner/datagenerator/distributions/DiscreteVariableParameter.java
+++ src/main/java/de/frosner/datagenerator/distributions/DiscreteVariableParameter.java
@@ -2,6 +2,8 @@ package de.frosner.datagenerator.distributions;
 
 import java.util.Map;
 
+import javax.annotation.Nonnull;
+
 import net.sf.qualitycheck.Check;
 
 import com.google.common.collect.Maps;
@@ -12,21 +14,20 @@ import de.frosner.datagenerator.features.FeatureValue;
 
 public class DiscreteVariableParameter<T> extends VariableParameter<T> {
 
-	private final Map<DiscreteFeatureValue, T> _parameterSelection;
+	private final Map<DiscreteFeatureValue, T> _featureValueParameterMapping;
 
-	public DiscreteVariableParameter(Map<DiscreteFeatureValue, T> parameterSelection) {
-		Check.notNull(parameterSelection);
-		_parameterSelection = Maps.newHashMap(parameterSelection);
+	public DiscreteVariableParameter(@Nonnull Map<DiscreteFeatureValue, T> featureValueParameterMapping) {
+		_featureValueParameterMapping = Maps.newHashMap(Check.notNull(featureValueParameterMapping));
 	}
 
 	@Override
 	public void updateParameter(FeatureValue value) {
 		Check.instanceOf(DiscreteFeatureValue.class, value, "value");
-		if (!_parameterSelection.containsKey(value)) {
+		if (!_featureValueParameterMapping.containsKey(value)) {
 			throw new FeatureValueCannotBeMappedException(value);
 		}
 
-		_parameter = _parameterSelection.get(value);
+		_parameter = _featureValueParameterMapping.get(value);
 	}
 
 }
